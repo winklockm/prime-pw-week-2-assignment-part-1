c5bac87cc3e66b79d19df3bc0c29116027f0983e
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-07-19 10:39:54 -0500

    created name variables

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,13 +1,13 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-
+let firstName = 'Maggie';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = 'Whitlock';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-
+let fullName = firstName + ' ' + lastName;
 // 4 - Console log the value of `fullName`
-
+console.log(fullName);
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 
 // 6 - Console log this sentence, adding in the variables you created above: 
@@ -29,7 +29,6 @@
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
 
-
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
 
@@ -38,6 +37,7 @@
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
+
 
 // STRETCH GOALS:
 
