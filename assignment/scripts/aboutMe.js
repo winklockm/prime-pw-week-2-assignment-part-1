083c3f45f3cf7e67a7aa51f10ae8665af03e2d7b
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-07-19 11:00:41 -0500

    created else if conditional

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -42,8 +42,15 @@ if(luckyNumber == 2 && adventurous){
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
-
-
+if(pets < allowedPets){
+    console.log('I can have more pets!');
+}
+else if(pets == allowedPets){
+    console.log('I have enough pets');
+}
+else if(pets > allowedPets){
+    console.log('Oh no, I have too many pets!');
+}
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that
