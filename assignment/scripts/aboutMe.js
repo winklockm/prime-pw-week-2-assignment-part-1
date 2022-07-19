814f9785202d707191f6dbe633bb1e31458e5fb5
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-07-19 10:52:28 -0500

    created allowedPets variable

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -26,7 +26,7 @@ let friendsPets = 1;
 // 11 - Add two pets to your `pets` variable
 pets += 2;
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+const allowedPets = 2;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
 
