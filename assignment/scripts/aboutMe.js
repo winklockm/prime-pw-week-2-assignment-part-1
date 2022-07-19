43d8084cfb5d79e82cf587135faa40553f1cf3f6
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-07-19 10:56:16 -0500

    created compound conditional

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -34,7 +34,9 @@ if(adventurous){
 }
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
-
+if(luckyNumber == 2 && adventurous){
+    console.log('Roll the dice!');
+}
 // 15 - Write a conditional that console logs "I can have more pets!" 
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
