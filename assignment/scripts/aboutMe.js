1907265daae2c4cc284c49349871b75d7112aca8
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-07-19 10:48:46 -0500

    created pets variables

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -20,9 +20,9 @@ let adventurous = true;
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 let food = 'hummus';
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-
+let pets = 2;
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+let friendsPets = 1;
 // 11 - Add two pets to your `pets` variable
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
