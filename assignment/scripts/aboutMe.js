66fa88ccce347ceb2e6324ea454d26e774e07499
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-07-20 12:47:26 -0500

    answered question 17

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -77,6 +77,20 @@ console.log(mostPets);
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
 
+switch(luckyNumber){
+    case 1:
+        console.log('First is the worst');
+        break;
+    case 2:
+        console.log('Second is the best');
+        break;
+    case 3:
+        console.log('Third is the one with the polka dot dress');
+        break;
+    default:
+        console.log('Luck is what happens when preparation meets opportunity');
+}
+
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
 
 
